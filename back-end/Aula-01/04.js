7680a1e88cc26839c1f143f73cd74a4f25964bab
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-15 19:51:43 -0300

    Finish aula 01

--- back-end/Aula-01/04.js
+++ back-end/Aula-01/04.js
@@ -5,40 +5,34 @@
 
 
 // 1-------------------------------
-let valor = 100;
+let valor = prompt("Digite o valor do produto: ");
 let desconto = valor * 0.2;
 let valorFinal = valor - desconto;
 
-console.log("\nO valor com desconto é:", valorFinal);
+alert("O valor com desconto é: " + valorFinal);
 // --------------------------------
 
 // 2-------------------------------
-let cel = 100;
+let cel = prompt("Digite o valor da temperatura em Celsius: ");
 let fah = (cel * 1.8) + 32
 
-console.log("\nA temperatura em Celsius convertida em Fahrenheit é:", fah);
+alert("A temperatura em Celsius convertida em Fahrenheit é: " + fah);
 // --------------------------------
 
 // 3-------------------------------
-let lados = 10;
-let perimetro = lados * 3
-let base = lados;
-let h = (lados * Math.sqrt(3)) / 2
-let area = (base * h ) / 2
+let base = prompt("Digite o valor da base do retângulo: ");
+let altura = prompt("Digite o valor da altura do retângulo: ");
+let perimetro = (base + altura) * 2
+let area = base * altura
 
-area = parseInt(area)
-
-console.log("\nO perímetro do triangulo é:", perimetro);
-console.log("O Área do triangulo é:", area);
+alert("O perímetro do retângulo é: " + perimetro);
+alert("O Área do retângulo é: " + area);
 // --------------------------------
 
 // 4-------------------------------
-let anoNascimento = 1991;
+let anoNascimento = prompt("Digite o ano do seu nascimento: ");
 let anoAtual = 2023;
 let idade = anoAtual - anoNascimento
 
-console.log("\nA idade do usuário é:", idade, "\n")
+alert("A idade do usuário é: " + idade)
 // --------------------------------
-
-
-
