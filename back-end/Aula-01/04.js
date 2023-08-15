65c2b7615e77774de8db6854690805135c990a09
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-15 15:37:04 -0300

    Update 04.js in aula 01

--- back-end/Aula-01/04.js
+++ back-end/Aula-01/04.js
@@ -9,21 +9,36 @@ let valor = 100;
 let desconto = valor * 0.2;
 let valorFinal = valor - desconto;
 
-console.log("O valor com desconto é:", valorFinal);
+console.log("\nO valor com desconto é:", valorFinal);
 // --------------------------------
 
 // 2-------------------------------
 let cel = 100;
 let fah = (cel * 1.8) + 32
 
-console.log("A temperatura em Celsius convertida em Fahrenheit é:", fah);
+console.log("\nA temperatura em Celsius convertida em Fahrenheit é:", fah);
 // --------------------------------
 
 // 3-------------------------------
-let lado = 20;
-let perimetro = lado * 3
+let lados = 10;
+let perimetro = lados * 3
+let base = lados;
+let h = (lados * Math.sqrt(3)) / 2
 let area = (base * h ) / 2
 
-console.log("O perímetro do triangulo é:", perimetro);
+area = parseInt(area)
+
+console.log("\nO perímetro do triangulo é:", perimetro);
 console.log("O Área do triangulo é:", area);
 // --------------------------------
+
+// 4-------------------------------
+let anoNascimento = 1991;
+let anoAtual = 2023;
+let idade = anoAtual - anoNascimento
+
+console.log("\nA idade do usuário é:", idade, "\n")
+// --------------------------------
+
+
+
