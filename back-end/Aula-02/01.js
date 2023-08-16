dce839c6fabbdbab09b174120afc3f463f16d692
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-16 19:41:04 -0300

    Update 01.js in aula 02

--- back-end/Aula-02/01.js
+++ back-end/Aula-02/01.js
@@ -12,13 +12,13 @@
 // let desconto = valorProduto * 0.2;
 // let resultadoIII = valorProduto - desconto;
 
-// if (mediaMensal < 500 & valorProduto < 30) {
+// if (mediaMensal < 500 && valorProduto < 30) {
 //     alert("O produto recebeu o aumento de 10%. Ficando: " + resultadoI);
 // } 
-// else if (mediaMensal >= 500 & mediaMensal < 1200 & valorProduto >= 30 & valorProduto < 80) {
+// else if (mediaMensal >= 500 && mediaMensal < 1200 && valorProduto >= 30 && valorProduto < 80) {
 //     alert("O produto recebeu o aumento de 15%. Ficando: " + resultadoII);
 // }
-// else if (mediaMensal >= 1200 & valorProduto >= 80) {
+// else if (mediaMensal >= 1200 && valorProduto >= 80) {
 //     alert("O produto recebeu o desconto de 20%. Ficando: " + resultadoIII);
 // }
 // else {
@@ -29,16 +29,16 @@
 let valorProduto = Number(prompt("Digite o valor do produto: "));
 let mediaMensal = Number(prompt("Digite o número da média mensal: "));
 
-if (mediaMensal < 500 & valorProduto < 30) {
+if (mediaMensal < 500 && valorProduto < 30) {
     valorProduto += valorProduto * 0.1;
     alert("O produto recebeu o aumento de 10%. Ficando: " + valorProduto);
 } 
-else if (mediaMensal >= 500 & mediaMensal < 1200 & valorProduto >= 30 & valorProduto < 80) {
+else if (mediaMensal >= 500 && mediaMensal < 1200 && valorProduto >= 30 && valorProduto < 80) {
     valorProduto += valorProduto * 0.15;
     parseFloat(valorProduto);
     alert("O produto recebeu o aumento de 15%. Ficando: " + valorProduto);
 }
-else if (mediaMensal >= 1200 & valorProduto >= 80) {
+else if (mediaMensal >= 1200 && valorProduto >= 80) {
     valorProduto -= valorProduto * 0.2;    
     alert("O produto recebeu o desconto de 20%. Ficando: " + valorProduto);
 }
