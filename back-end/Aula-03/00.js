118e9df4490b878cb3c20a4e1cb09cfa9b998a44
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-16 19:41:31 -0300

    Create aula 03 for backend

---
+++ back-end/Aula-03/00.js
@@ -0,0 +1,40 @@
+// 1-------------------------------
+let dia = Number(prompt("Digite um número em relação ao dia da semana: "));
+
+switch (dia) {
+
+    case 1:
+        alert("É Domingo!");
+        break;
+
+    case 2:
+        alert("É Segunda!");
+        break;
+
+    case 3:
+        alert("É Terça!");
+        break;
+
+    case 4:
+        alert("É Quarta!");
+        break;
+
+    case 5:
+        alert("É Quinta!");
+        break;
+
+    case 6:
+        alert("É Sexta!");
+        break;
+
+    case 7:
+        alert("É Sábado!");
+        break;
+
+    default:
+        alert("Erro! Digite novamente...");
+}
+
+
+
+// --------------------------------
