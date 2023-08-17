4e4b3fe17fd34a00e1b42bccc3d3304ad4db4ae4
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-16 21:14:28 -0300

    Update 00.js in aula 03

--- back-end/Aula-03/00.js
+++ back-end/Aula-03/00.js
@@ -1,40 +1,36 @@
 // 1-------------------------------
-let dia = Number(prompt("Digite um número em relação ao dia da semana: "));
+// let dia = Number(prompt("Digite um número em relação ao dia da semana: "));
 
-switch (dia) {
+function diaSemana() {
 
-    case 1:
-        alert("É Domingo!");
-        break;
+    const dia = Number(document.getElementById("number").value);
 
-    case 2:
-        alert("É Segunda!");
-        break;
-
-    case 3:
-        alert("É Terça!");
-        break;
-
-    case 4:
-        alert("É Quarta!");
-        break;
-
-    case 5:
-        alert("É Quinta!");
-        break;
-
-    case 6:
-        alert("É Sexta!");
-        break;
-
-    case 7:
-        alert("É Sábado!");
-        break;
-
-    default:
-        alert("Erro! Digite novamente...");
+    switch (dia) {
+        case 1:
+            alert("É Domingo!");
+            break;
+        case 2:
+            alert("É Segunda!");
+            break;
+        case 3:
+            alert("É Terça!");
+            break;
+        case 4:
+            alert("É Quarta!");
+            break;
+        case 5:
+            alert("É Quinta!");
+            break;
+        case 6:
+            alert("É Sexta!");
+            break;
+        case 7:
+            alert("É Sábado!");
+            break;
+        default:
+            alert("Erro! Digite novamente...");
+    }
 }
-
-
-
 // --------------------------------
+
+
