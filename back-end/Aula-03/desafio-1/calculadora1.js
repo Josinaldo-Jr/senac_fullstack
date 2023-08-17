67c4b318f820a5ea9a6022bf68c9b7b17e0db8e7
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-17 17:14:04 -0300

    Add desafio 1 and files

---
+++ back-end/Aula-03/desafio-1/calculadora1.js
@@ -0,0 +1,55 @@
+// function calcular() {
+//     const num1 = parseFloat(document.getElementById("num1").value);
+//     const num2 = parseFloat(document.getElementById("num2").value);
+//     const operacao = document.getElementById("operacao").value;
+//     let resultado;
+
+//     switch (operacao) {
+//       case "somar":
+//         resultado = num1 + num2;
+//         break;
+//       case "subtrair":
+//         resultado = num1 - num2;
+//         break;
+//       case "multiplicar":
+//         resultado = num1 * num2;
+//         break;
+//       case "sectionidir":
+//         resultado = num1 / num2;
+//         break;
+//       default:
+//         resultado = "Operação inválida";
+//     }
+
+//     document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+//   }
+
+function calculadora() {
+    const numA = parseFloat(document.getElementById("numA").value);
+    const numB = parseFloat(document.getElementById("numB").value);
+    const operar = document.getElementById("operar").value;
+    // let result;
+
+    switch (operar) {
+        case "somar":
+            alert(numA + numB)
+            break;
+
+        case "subtrair":
+            alert(numA - numB)
+            break;
+
+
+        case "multiplicar":
+            alert(numA * numB)
+            break;
+
+        case "dividir":
+            alert(numA / numB)
+            break;
+
+        default:
+            alert("Erro! Digite novamente...")
+            break;
+    }
+}
