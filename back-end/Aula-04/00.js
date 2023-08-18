12df36d26c87ff5343e6928d971893222582352c
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-17 21:15:10 -0300

    Add aula 04 in backand

---
+++ back-end/Aula-04/00.js
@@ -0,0 +1,7 @@
+// 1-------------------------------
+function inserir(entrada){
+    let visor = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML =  visor + entrada;
+}
+
+// --------------------------------
