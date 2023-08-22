69835e0e5728ef0f75c181273687e99ecb7c3602
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-21 21:14:26 -0300

    Update aula 05 in backand

---
+++ back-end/Aula-05/00.js
@@ -0,0 +1,19 @@
+// 1-------------------------------
+alert("Escolha números referentes a x/y/z");
+
+let x = parseInt(prompt("Digite um número inteiro para x: "));
+let y = parseInt(prompt("Digite um número inteiro para y: "));
+let z = parseInt(prompt("Digite um número inteiro para z: "));
+
+alert("---Menu----\nEscolha uma operação\n0: Sair\n1: Média Geométrica\n2: Média Ponderada\n3: Média Aritmética");
+
+// do {
+
+//     chico = chico + 2;
+//     ze = ze + 3;
+//     ano = ano + 1;     
+
+//     console.log(ano);
+    
+// } while (ze < chico);
+// --------------------------------
---
+++ back-end/Aula-05/01.js
@@ -0,0 +1,15 @@
+// 1-------------------------------
+let chico = 150;
+let ze = 110;
+let ano = 0;
+
+do {
+
+    chico = chico + 2;
+    ze = ze + 3;
+    ano = ano + 1;     
+
+    console.log(ano);
+    
+} while (ze < chico);
+// --------------------------------
