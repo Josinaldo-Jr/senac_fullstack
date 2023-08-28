8597b5f34b10b0af2a095b5b41aa15f41cc6cc35
Josinaldo-Jr <josinaldoeuzebiorjr@gmail.com>
2023-08-28 12:19:06 -0300

    Update files in backand

--- back-end/Aula-08/teste.js
+++ back-end/Aula-08/teste.js
@@ -1,3 +1,18 @@
 
 
    
+let a = [1,2,3,2,2,2,5,7,9,2,4,6,3,5,7];
+let b = [];
+
+let soma = 0;
+
+for(let i = 0; i < a.length; i++) {
+
+    soma+= a[i];
+    if( (i+1) % 3 === 0) {
+        let adicionar = b.push(soma/3);
+        soma = 0;
+    }
+}
+
+document.write(b);
