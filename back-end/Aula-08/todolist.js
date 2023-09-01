0e9a49f7c5bfc0dc0b6f16ebe65e846b6b17924d
Josinaldo-Jr <josinaldoeuzebiorjr@gmail.com>
2023-09-01 11:24:49 -0300

    Finish todolist of aula 08 in backand

---
+++ back-end/Aula-08/todolist.js
@@ -0,0 +1,40 @@
+
+const lista = [];
+
+function adicionar() {
+    const entrada = document.getElementById("entrada").value;
+
+    if (entrada.trim() !== "") {
+        lista.push(entrada);
+    }
+
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+
+    lista.forEach(function (item, indice) {
+        result.innerHTML += `
+                 <li>
+                     <span id="tarefa-${indice}">${item}</span>
+                     <button class="edit-button" onclick="editar(${indice})">Editar</button>
+                     <button class="delete-button" onclick="deletar(${indice})">Deletar</button>
+                 </li>
+                `;
+    });
+
+    document.getElementById("entrada").value = ""; // Limpa o campo de entrada após adicionar uma tarefa.
+}
+
+function deletar(indice) {
+    lista.splice(indice, 1);
+    adicionar(); // Atualiza a lista após a exclusão.
+}
+
+function editar(indice) {
+    const tarefa = document.getElementById(`tarefa-${indice}`);
+    const novoTexto = prompt("Editar tarefa:", tarefa.textContent);
+    if (novoTexto !== null) {
+        lista[indice] = novoTexto;
+        adicionar(); // Atualiza a lista após a edição.
+    }
+}
+
