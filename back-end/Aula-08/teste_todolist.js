4026a824d7e0155c63986262148a473f6f47095d
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-09-04 16:40:57 -0300

    Update teste_todolista.js

--- back-end/Aula-08/teste_todolist.js
+++ back-end/Aula-08/teste_todolist.js
@@ -12,17 +12,57 @@
 // aplicando o valor de result para o html = ""
 
 // aplicar um foreach na "lista" com function (item, indice):
-// variavel result soma e recebe a lista
+// variavel result soma e recebe a lista ao html
 // fecha a foreach
 
-// limpara "entrada" com valor vazio
+// limpar o document "entrada" com valor vazio
 
 // fecha a função adicionar
 // --------------------------------------------
 
+// função deletar(valor do indice)
+// lista com slice(indice, 1) valor indice e 1 item deletado
+// chama a função adcionar para atualizar a lista
+// fecha a função deletar
+// --------------------------------------------
+
+// função editar(valor indice)
+
+// variavel (nome) para acessar o documento e a id tarefa(foi criada) + indice
+
+// variavel para o novo item (solicitando em prompt, dentro chamar a variavel.textContent (mostrar o conteúdo que será editado))
+
+// condicional
+
 const lista = [];
 
 function adicionar() {
+
     let entrada = document.getElementById("entrada").value;
 
+    if(entrada.trim() !== ""){
+        lista.push(entrada);
+    }
+
+    let result = document.getElementById("result");
+    result.innerHTML = "";
+
+    lista.forEach(function (item, indice){
+        result.innerHTML+=   
+    `<li>
+        <span id="tarefa-${indice}">${item}</span>
+        <button class="edit-button" onclick="editar(${indice})">Editar</button>
+        <button class="delete-button" onclick="deletar(${indice})">Deletar</button>
+    </li>
+    `;        
+
+    });
+
+    document.getElementById("entrada").value = ""; 
 }
+
+function deletar (indice) {
+    lista.splice(indice, 1);
+    adicionar();
+}
+
