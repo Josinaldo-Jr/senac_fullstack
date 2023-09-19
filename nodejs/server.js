6adfa29bed0a24a581404815a7c0769b0394c8c3
Josinaldo-Jr <josinaldoeuzebiorjr@gmail.com>
2023-09-19 16:32:36 -0300

    Create crud with fastify in nodejs folder

---
+++ nodejs/server.js
@@ -0,0 +1,65 @@
+// import { response } from 'express';
+// import { createServer, request } from 'node:http';
+
+// const server = createServer((request, response) => {
+//     response.write("hello");
+//     return response.end();
+// });
+
+// server.listen(3333);
+// ----------------------
+
+import { fastify } from "fastify";
+import { DatabaseMemory } from './database-memory.js';
+import { request, response } from "express";
+
+const server = fastify();
+
+const database = new DatabaseMemory();
+
+server.post('/videos', (request, response) => {
+    const { title, description, duration } = request.body;
+
+    database.create ({
+        title,
+        description,
+        duration,
+    });
+
+    return response.status(201).send();
+});
+
+server.get('/videos', (request) => {
+    const search = request.query.search;
+
+    const videos = database.list(search);
+
+    return videos;
+});
+
+server.put('/videos/:id', (request, response) => {
+    const videoId = request.params.id;
+
+    const { title, description, duration } = request.body;
+
+    database.update(videoId, { 
+        title,
+        description,
+        duration,
+    });
+
+    return response.status(204).send();
+
+});
+
+server.delete('/videos/:id', (request, response) => {
+    const videoId = request.params.id;
+
+    database.delete(videoId);
+
+    return response.status(204).send();    
+});
+
+server.listen({
+    port: 3333,
+});
