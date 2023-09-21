4dede90c771525323684477a2d825ab941cdcbf1
Josinaldo-Jr <josinaldoeuzebiorjr@gmail.com>
2023-09-21 11:54:58 -0300

    Update crud and dabase for nodejs folder

--- nodejs/server.js
+++ nodejs/server.js
@@ -10,17 +10,19 @@
 // ----------------------
 
 import { fastify } from "fastify";
-import { DatabaseMemory } from './database-memory.js';
+// import { DatabaseMemory } from './database-memory.js';
+import { DatabasePostgres } from './database-postgres.js';
 import { request, response } from "express";
 
 const server = fastify();
 
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 
-server.post('/videos', (request, response) => {
+server.post('/videos', async (request, response) => {
     const { title, description, duration } = request.body;
 
-    database.create ({
+    await database.create ({
         title,
         description,
         duration,
@@ -29,20 +31,20 @@ server.post('/videos', (request, response) => {
     return response.status(201).send();
 });
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
     const search = request.query.search;
 
-    const videos = database.list(search);
+    const videos = await database.list(search);
 
     return videos;
 });
 
-server.put('/videos/:id', (request, response) => {
+server.put('/videos/:id', async (request, response) => {
     const videoId = request.params.id;
 
     const { title, description, duration } = request.body;
 
-    database.update(videoId, { 
+    await database.update(videoId, { 
         title,
         description,
         duration,
@@ -52,10 +54,10 @@ server.put('/videos/:id', (request, response) => {
 
 });
 
-server.delete('/videos/:id', (request, response) => {
+server.delete('/videos/:id', async (request, response) => {
     const videoId = request.params.id;
 
-    database.delete(videoId);
+   await database.delete(videoId);
 
     return response.status(204).send();    
 });
