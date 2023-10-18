b02a7dc1b7a313be855f38db553f4cc69a27b86a
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-10-18 17:05:43 -0300

    Update routes in projeto final

--- projeto-final/Its-my-learn/app.js
+++ projeto-final/Its-my-learn/app.js
@@ -20,5 +20,20 @@ app.get('/sobre', (req, res) => {
 	res.render("sobre", {titulo:"Sobre"});
 });
 
+//Rota para atv1
+app.get('/atv1', (req, res) => {
+	res.render("atv1", {titulo:"Atividade 01"});
+});
+
+//Rota para atv2
+app.get('/atv2', (req, res) => {
+	res.render("atv2", {titulo:"Atividade 02"});
+});
+
+//Rota para atv3
+app.get('/atv3', (req, res) => {
+	res.render("atv3", {titulo:"Atividade 03"});
+});
+
 // Cria um servidor 
 app.listen(3000, () => console.log('server is running'));
