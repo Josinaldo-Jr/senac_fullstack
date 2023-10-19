b4ac188d7bf4f3705f1de890efa5011bb5a18ffe
Josinaldo-Jr <josinaldoeuzebiorjr@gmail.com>
2023-10-18 23:00:19 -0300

    Transform dynamic elements in projeto final

--- projeto-final/Its-my-learn/app.js
+++ projeto-final/Its-my-learn/app.js
@@ -10,6 +10,11 @@ app.use(express.static('public'));
 
 
 //Rotas:
+//Rota para favicon
+app.get('/LogoIcone.png', (req, res) => {
+	res.sendFile(__dirname + '/public/img/LogoIcone.png');
+  });
+  
 //Rota para raiz
 app.get('/', (req, res) => {
 	res.render("index", {titulo:"Início"});
@@ -18,6 +23,11 @@ app.get('/', (req, res) => {
 //Rota para sobre
 app.get('/sobre', (req, res) => {
 	res.render("sobre", {titulo:"Sobre"});
+});
+
+//Rota para my learns
+app.get('/mylearns', (req, res) => {
+	res.render("mylearns", {titulo:"My Learns"});
 });
 
 //Rota para atv1
@@ -34,6 +44,22 @@ app.get('/atv2', (req, res) => {
 app.get('/atv3', (req, res) => {
 	res.render("atv3", {titulo:"Atividade 03"});
 });
+
+//Rota para login
+app.get('/login', (req, res) => {
+	res.render("login", {titulo:"Login"});
+});
+
+//Rota para cadastro
+app.get('/cadastro', (req, res) => {
+	res.render("cadastro", {titulo:"Cadastro do Usuário"});
+});
+
+//Rota para esqueceu a senha
+app.get('/esqueceu_senha', (req, res) => {
+	res.render("esqueceu_senha", {titulo:"Esqueci a Senha"});
+});
+
 
 // Cria um servidor 
 app.listen(3000, () => console.log('server is running'));
